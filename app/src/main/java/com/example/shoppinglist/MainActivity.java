4373aab889fb8b5e980dfc511b400dee1987b43f
Cynthia Randriamanohisoa <cynthiarandria@fb.com>
2020-06-27 16:33:58 -0700

    Add list recycler view with dummy items

--- app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.shoppinglist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements ShoppingListViewAdapter.ItemClickListener {
+
+    ShoppingListViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // data to populate the RecyclerView with
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Barres");
+        animalNames.add("Cereales cuicui");
+        animalNames.add("Persil lapin");
+        animalNames.add("Eau gallon");
+        animalNames.add("Lardons");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.main_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new ShoppingListViewAdapter(this, animalNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    // TODO see if I need this functionality
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 }

5faf8e9a508e87d3803563d08f3061cd6752fa21
Cynthia Randriamanohisoa <cynthiarandria@fb.com>
2020-06-29 17:28:22 -0700

    Add navigation drawer

--- app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,10 +20,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.shoppinglist.model.Item;
 import com.example.shoppinglist.model.ShoppingList;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements ShoppingListViewAdapter.OnItemCheckListener, AddFragment.AddItemDialogListener {
 
+    private ActionBarDrawerToggle drawerToggle;
     private ShoppingListViewAdapter adapter;
     private ShoppingList shoppingList;
 
@@ -28,6 +35,33 @@ public class MainActivity extends AppCompatActivity implements ShoppingListViewA
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DrawerLayout drawerLayout = findViewById(R.id.activity_main);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int id = item.getItemId();
+                switch(id)
+                {
+                    case R.id.nav_shopping_list:
+                        Toast.makeText(MainActivity.this, "Shopping list",Toast.LENGTH_SHORT).show();break;
+                    case R.id.nav_catalog:
+                        Toast.makeText(MainActivity.this, "Catalog",Toast.LENGTH_SHORT).show();break;
+                    default:
+                        return true;
+                }
+                return true;
+            }
+        });
 
         String filename = "listFile";
         FileService fileService = new FileService(this);
@@ -69,6 +103,15 @@ public class MainActivity extends AppCompatActivity implements ShoppingListViewA
         fileService.writeToFile(filename, shoppingList);
 
         super.onStop();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(drawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
